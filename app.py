28e26183a0455afb20b87c049c17f9526359496b
Ismail <iskilicaslan61@gmail.com>
2025-07-14 14:02:33 +0200

    first commit

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ def send_email():
 def health():
     return jsonify({"message": "English Level Placement Test Backend is running."})
 # Uygulamayı çalıştır
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+#if __name__ == '__main__':
+#    app.run(debug=True, host='0.0.0.0', port=5000)
